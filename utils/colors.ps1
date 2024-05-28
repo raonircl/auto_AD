77ed637d121a9fcc387c31ba7673d6d1449d4dda
raonircl <raoni.afi@gmail.com>
2024-05-28 11:47:28 -0300

    module to format with colors

---
+++ utils/colors.ps1
@@ -0,0 +1,24 @@
+function Write-Blue {
+    param (
+        [string]$text
+    )
+    write-Host $text -ForegroundColor Blue
+}
+function Write-Red {
+    param (
+        [string]$text
+    )
+    write-Host $text -ForegroundColor Red
+}
+function Write-Green {
+    param (
+        [string]$text
+    )
+    write-Host $text -ForegroundColor Green
+}
+function Write-Yellow {
+    param (
+        [string]$text
+    )
+    write-Host $text -ForegroundColor Yellow
+}
